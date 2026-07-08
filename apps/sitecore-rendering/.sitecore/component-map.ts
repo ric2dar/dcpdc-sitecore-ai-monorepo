d74a1bfaed19cd7f32d050b3ae04a660269bad3d
ric2dar <rdaroyaii@deloitte.com>
2026-07-09 03:21:43 +0800

    header.tsx

--- apps/sitecore-rendering/.sitecore/component-map.ts
+++ apps/sitecore-rendering/.sitecore/component-map.ts
@@ -14,6 +14,7 @@ import * as PageContent from 'src/components/page-content/PageContent';
 import * as Navigation from 'src/components/navigation/Navigation';
 import * as LinkList from 'src/components/link-list/LinkList';
 import * as Image from 'src/components/image/Image';
+import * as Header from 'src/components/header/Header';
 import * as ContentBlock from 'src/components/content-block/ContentBlock';
 import * as Container from 'src/components/container/Container';
 import * as ColumnSplitter from 'src/components/column-splitter/ColumnSplitter';
@@ -32,6 +33,7 @@ export const componentMap = new Map<string, NextjsContentSdkComponent>([
   ['Navigation', { ...Navigation, componentType: 'client' }],
   ['LinkList', { ...LinkList }],
   ['Image', { ...Image }],
+  ['Header', { ...Header }],
   ['ContentBlock', { ...ContentBlock, componentType: 'client' }],
   ['Container', { ...Container }],
   ['ColumnSplitter', { ...ColumnSplitter }],
